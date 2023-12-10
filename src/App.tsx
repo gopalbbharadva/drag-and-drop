3a70e16cb7a4e4252d4bad757a17e2cb86ad27f5
gopalbbharadva <gopalbbharadva@gmail.com>
2023-12-10 12:28:49 +0530

    Feat: delete draggable item

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,149 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './App.module.css'
+import { Modal } from './components/Modal'
+
+type ElementType = {
+  xPoint: number
+  yPoint: number
+  text: string
+  id: string
+}
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [isDragging, setIsDragging] = useState(false)
+  const [showModal, setShowModal] = useState(false)
+  const [currentElementId, setCurrentElementId] = useState<string | undefined>(
+    ''
+  )
 
-  return <div>Hello vite</div>
+  const closeModalHandler = () => {
+    setShowModal((prev) => !prev)
+  }
+  const [elementsArr, setElementsArr] = useState([] as ElementType[])
+
+  const dragStart = (event: any, id?: string) => {
+    event.dataTransfer.setData('text/plain', 'draggable')
+    event.dataTransfer.effectAllowed = 'move'
+    setIsDragging(true)
+    setCurrentElementId(id)
+  }
+
+  const dragOverHandler = (event: any) => {
+    event.preventDefault()
+    if (isDragging) {
+      const x = event.clientX
+      const y = event.clientY
+      setPosition({ x, y })
+    }
+  }
+
+  const saveHandler = (e: any, textInput: string) => {
+    console.log('submit')
+    e.preventDefault()
+    setElementsArr((prev) =>
+      prev.find((item) => item.id === currentElementId)
+        ? prev.map(
+            (element) =>
+              element.id === currentElementId
+                ? {
+                    ...element,
+                    xPoint: position.x,
+                    yPoint: position.y,
+                    text: textInput,
+                  }
+                : element,
+            []
+          )
+        : [
+            ...prev,
+            {
+              id: crypto.randomUUID(),
+              xPoint: position.x,
+              yPoint: position.y,
+              text: textInput,
+            },
+          ]
+    )
+    setShowModal((prev) => !prev)
+  }
+
+  const dragEndHandler = () => {
+    setIsDragging(false)
+    setShowModal(true)
+  }
+  console.log(showModal, 'showModal')
+
+  const handleKeyPress = (event: any, id: string) => {
+    // Check if the pressed key is Enter (key code 13)
+    if (event.key === 'Enter') {
+      setShowModal((prev) => !prev)
+    }
+    if (event.key === 'Delete') {
+      setCurrentElementId('')
+      setElementsArr((prev) => prev.filter((item) => item.id !== id))
+      // Call your click handler function here
+      // console.log('enter key clicked')
+      // handleClick()
+    }
+  }
+
+  return (
+    <div className={styles.container}>
+      {showModal && (
+        <Modal
+          submitFormHandler={saveHandler}
+          closeHandler={closeModalHandler}
+          itemInfo={{
+            xAxis: position.x,
+            yAxis: position.y,
+            text: elementsArr.find((item) => item.id === currentElementId)
+              ? elementsArr.find((item) => item.id === currentElementId)?.text
+              : '',
+          }}
+        />
+      )}
+      <div onDragOver={dragOverHandler} className={styles.blankContainer}>
+        {/* white part */}
+        {elementsArr.map(({ text, xPoint, yPoint, id }) => (
+          <div
+            key={id}
+            onKeyDown={(e) => handleKeyPress(e, id)}
+            onClick={() => setCurrentElementId(id)}
+            id={id}
+            tabIndex={0}
+            draggable='true'
+            onDragStart={(e) => dragStart(e, id)}
+            onDragEnd={dragEndHandler}
+            style={{
+              border: id === currentElementId ? '1px solid red' : '',
+              position: 'absolute',
+              left: `${xPoint}px`,
+              top: `${yPoint}px`,
+              cursor: 'grab',
+              color: 'black',
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <div className={styles.rightPart}>
+        <div
+          id='draggable'
+          draggable='true'
+          onDragStart={dragStart}
+          onDragEnd={dragEndHandler}
+          style={{
+            cursor: 'grab',
+            color: 'white',
+          }}
+        >
+          Drag me
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/Modal.tsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import styles from './Modal.module.css'
+
+type ModalPropsType = {
+  itemInfo: { xAxis: number; yAxis: number; text: any }
+  closeHandler: () => void
+  submitFormHandler: (e: any, text: string, id: string) => void
+}
+
+export const Modal = ({
+  itemInfo,
+  closeHandler,
+  submitFormHandler,
+}: ModalPropsType) => {
+  const { xAxis, yAxis } = itemInfo
+  const [formState, setFormState] = useState({
+    xAxis,
+    yAxis,
+    text: itemInfo.text,
+  })
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setFormState((prev) => ({ ...prev, [name]: value }))
+  }
+
+  console.log(formState.text, 'formState')
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.modal}>
+        <p>Edit Label</p>
+        <button onClick={closeHandler} className={styles.closeButton}>
+          X
+        </button>
+        <form
+          onSubmit={(e) => submitFormHandler(e, formState.text, '')}
+          className={styles.modalForm}
+        >
+          <label htmlFor='x-axis'>
+            Text
+            <input
+              autoFocus
+              value={formState.text}
+              onChange={changeHandler}
+              type='text'
+              id='text'
+              name='text'
+              placeholder='This is a label'
+            />
+          </label>
+          <label htmlFor='x-axis'>
+            X
+            <input
+              value={formState.xAxis}
+              onChange={changeHandler}
+              type='number'
+              id='x-axis'
+              name='xAxis'
+            />
+          </label>
+          <label htmlFor='y-axis'>
+            Y
+            <input
+              value={formState.yAxis}
+              onChange={changeHandler}
+              type='number'
+              id='y-axis'
+              name='yAxis'
+            />
+          </label>
+          <button
+            type='submit'
+            className={`${styles.resetButton} ${styles.saveButton}`}
+          >
+            Save Changes
+          </button>
+        </form>
+      </div>
+    </div>
+  )
+}
